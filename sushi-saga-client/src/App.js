f500593f56fe7cd893b15683ab4a96cc8ad17250
iambryanhaney <i.am.bryan.haney@gmail.com>
2020-06-30 12:02:47 -0500

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,17 +1,52 @@
 import React, { Component } from 'react';
 import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
+import Wallet from './components/Wallet';
 
 // Endpoint!
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  constructor() {
+    super()
+    this.state = {
+      sushi: [],
+      conveyorPosition: 0,
+      budget: 200
+    }
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(resp => resp.json())
+    .then(json => this.setState({sushi: json}))
+    .catch(err => console.error(err))
+  }
+
+  incrementConveyor = () => {
+    this.setState({ conveyorPosition: (this.state.conveyorPosition+1) % 25})
+  }
+
+  eatSushi = (sushi) => {
+    if (this.state.budget >= sushi.price && sushi.eaten === undefined) {
+      sushi.eaten = true
+      this.setState({ budget: this.state.budget - sushi.price })
+    }
+  }
+
+  addMoney = (event, amount) => {
+    event.preventDefault()
+    this.setState({ budget: this.state.budget + parseInt(amount) })
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer sushi={this.state.sushi.slice(this.state.conveyorPosition*4, this.state.conveyorPosition*4+4)}
+          incrementConveyor={this.incrementConveyor} eatSushi={this.eatSushi} />
+        <Table budget={this.state.budget}/>
+        <Wallet addMoney={this.addMoney} />
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,17 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
   return (
     <Fragment>
       <div className="belt">
         {
-          /* 
-             Render Sushi components here!
-          */
+          props.sushi.map(sushi => {
+            return <Sushi sushi={sushi} eatSushi={props.eatSushi}/>
+          })
         }
-        <MoreButton />
+        <MoreButton incrementConveyor={props.incrementConveyor}/>
       </div>
     </Fragment>
   )
